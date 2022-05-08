0de0a03e72bff9a17a937f09e924358c66d25727
Alvarino <83028364+Rafael-A8@users.noreply.github.com>
2022-05-08 18:18:41 -0300

    Finalizando registro de tarefas
    Foi aplicado um Elias  para assim fazer um left join juntando dos os registros de tarefa com seus respectivos status
    left join tb_status as s on (t.id_status = s.id) dessa forma ficou cruzado o tb_status com status no banco de dados

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -1,11 +1,11 @@
 <?php
 	$acao = 'recuperar';
 	require 'tarefa-controller.php';
-
+/*
 	echo '<pre>';
 	print_r($tarefas);
 	echo '</pre>';	
-
+*/
 ?>
 <html>
 	<head>
@@ -42,31 +42,22 @@
 					<div class="container pagina">
 						<div class="row">
 							<div class="col">
-								<h4>Todas tarefas</h4>
+								<h4>Todas tarefas</h4>							
 								<hr />
-
-								<? foreach($tarefas as $indice => $tarefa) { ?>
+								<?php foreach($tarefas as $indice => $tarefa) { ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9">Lavar o carro (status)</div>
+										<div class="col-sm-9">
+											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
+										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
 											<i class="fas fa-edit fa-lg text-info"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
 								</div>
-								<? } ?>
 
-								
+								<?php } ?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
-								
 							</div>
 						</div>
 					</div>
